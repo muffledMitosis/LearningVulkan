380c5e0c3dced27ba37d5058e981cb6ba97c3510
Meth Munindradasa <methsndr@gmail.com>
2023-05-20 17:40:57 +0530

    Validation Layers

--- LearningVulkan/main.cpp
+++ LearningVulkan/main.cpp
@@ -5,9 +5,21 @@
 #include <vector>
 #include <stdexcept>
 #include <cstdlib>
+#include <cstring>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+
+const std::vector<const char*> validationLayers = {
+  "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+  const bool enableValidationLayers = false;
+#else
+  const bool enableValidationLayers = true;
+#endif // NDEBUG
+
 
 class HelloTriangleApplication {
 private:
@@ -24,8 +36,44 @@ public:
   }
 
 private:
+  bool checkValidationLayerSupport()
+  {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for (const char* layerName : validationLayers)
+    {
+      bool layerFound = false;
+
+      for (const auto& layerProperties : availableLayers)
+      {
+        if (strcmp(layerName, layerProperties.layerName) == 0)
+        {
+          layerFound = true;
+          break;
+        }
+      }
+
+      if (!layerFound)
+      {
+        std::cout << layerName << " NOT FOUND\n";
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   void createInstace()
   {
+    if (enableValidationLayers && !checkValidationLayerSupport())
+    {
+      throw std::runtime_error("validation layers requested but not available!");
+    }
+
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = "Hello Triangle";
@@ -37,6 +85,16 @@ private:
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_EVENT_CREATE_INFO;
     createInfo.pApplicationInfo = &appInfo;
+
+    if (enableValidationLayers)
+    {
+      createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+      createInfo.ppEnabledLayerNames = validationLayers.data();
+    }
+    else
+    {
+      createInfo.enabledLayerCount = 0;
+    }
 
     uint32_t glfwExtensionCount = 0;
     const char** glfwExtensions;
